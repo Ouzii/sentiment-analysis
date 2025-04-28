01fec9cac747fa8cf9a5d8eb8b14a2452798db02
Oskari Laaja <oskari.laaja@gmail.com>
2025-04-28 21:51:35 +0300

    Add empty input folder error

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,6 +1,10 @@
-#!bin/bash
+#!/usr/bin/env bash
 
 inputpath=$(pwd)/input
+if [ -z "$( ls -A $inputpath )" ]; then
+    printf 'Error: No inputs found' >&2
+    exit 1
+fi
 cd senti-classify
 for FILE in "$inputpath"/*.csv; do
     python predict.py $1 $2 $FILE $3 $4;
